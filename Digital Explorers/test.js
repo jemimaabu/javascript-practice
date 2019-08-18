3039ac01e2aecee3f26d4c0e66f62f98b860c36d
Jemima Abu <jemimaabu@gmail.com>
2019-08-18 17:58:02 +0100

    Formatted

--- Digital Explorers/test.js
+++ Digital Explorers/test.js
@@ -1,4 +1,28 @@
-var assert = require("assert");
+var assert = require('assert');
+
+function fizzBuzz(num) {
+  return (num % 3 === 0 ? "Fizz" : "") + (num % 5 === 0 ? "Buzz" : "") || num;
+}
+
+describe('FizzBuzz', function () {
+  it('should work', function () {
+    assert.deepEqual(fizzBuzz(1),1);
+    assert.deepEqual(fizzBuzz(2),2);
+    assert.deepEqual(fizzBuzz(3),"Fizz");
+    assert.deepEqual(fizzBuzz(4),4);
+    assert.deepEqual(fizzBuzz(5),"Buzz");
+    assert.deepEqual(fizzBuzz(6),"Fizz");
+    assert.deepEqual(fizzBuzz(7),7);
+    assert.deepEqual(fizzBuzz(8),8);
+    assert.deepEqual(fizzBuzz(9),"Fizz");
+    assert.deepEqual(fizzBuzz(10),"Buzz");
+    assert.deepEqual(fizzBuzz(11),11);
+    assert.deepEqual(fizzBuzz(12),"Fizz");
+    assert.deepEqual(fizzBuzz(13),13);
+    assert.deepEqual(fizzBuzz(14),14);
+    assert.deepEqual(fizzBuzz(15),"FizzBuzz");
+  });
+});
 
 function factorize(number) {
   var primes = [];
--- Practice.js
+++ Practice.js
@@ -1,271 +1,303 @@
 /* Returns the number of times a character occurs in a string */
 function numCount(arr, element) {
-	// numCount("hello, l") return 2l
-	var count = 0;
-	for (var i = 0; i<arr.length; i++){
-    	if (arr[i]===element) {
-		 	count++
-		}
-	}
-	return `${count}${element}`;
+  // numCount("hello, l") return 2l
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
+      count++
+    }
+  }
+  return `${count}${element}`;
 }
 
 /* Uses numCount function to return run length encoding of all characters in a string  */
 function runLengthEncoding(str) {
-	// runLengthEncoding("Hello World") returns 1h1e3l2o1w1r1d
-	var stringArray = str.toLowerCase().replace(/[^a-z]/g,"").split("");
-	var uniqueStringArray = [... new Set(stringArray)]
-	return uniqueStringArray.map((a) => numCount(stringArray, a)).join("");
+  // runLengthEncoding("Hello World") returns 1h1e3l2o1w1r1d
+  var stringArray = str.toLowerCase().replace(/[^a-z]/g, "").split("");
+  var uniqueStringArray = [... new Set(stringArray)]
+  return uniqueStringArray.map((a) => numCount(stringArray, a)).join("");
 }
 
 /* Reverses words in a sentence */
 function reverseSentence(str) {
-	// reverseSentence("Hello World") returns olleH dlroW
-	return str.split(" ").map(a => a.split("").reverse().join("")).join(" ");
+  // reverseSentence("Hello World") returns olleH dlroW
+  return str.split(" ").map(a => a.split("").reverse().join("")).join(" ");
 }
 
 /* Determines if a string is a pangram or not */
 function pangram(str) {
-	// If a set of the string has exactly 26 letters which are all the letters of the English alphabet, it's a pangram
-	const stringArray = str.toLowerCase().replace(/[^a-z]/g,"").split("");
-	const uniqueLetters = [... new Set(stringArray)];
-	return uniqueLetters.length === 26 ? "Pangram" : "Not pangram"
+  // If a set of the string has exactly 26 letters which are all the letters of the English alphabet, it's a pangram
+  const stringArray = str.toLowerCase().replace(/[^a-z]/g, "").split("");
+  const uniqueLetters = [... new Set(stringArray)];
+  return uniqueLetters.length === 26 ? "Pangram" : "Not pangram"
 }
 
 /* Returns cardinality of palindromes in array of strings input */
 function threatDetector(textMessages) {
 
-    function isPalindrome(s) {
-        s = s.trim().split("")
-        if (s.join("") === s.reverse().join("")) {
-            return true;
-        } else {
-            return false;
-        }
+  function isPalindrome(s) {
+    s = s.trim().split("")
+    if (s.join("") === s.reverse().join("")) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    function palindromeArray(str) {
-        var substrings = [];
+  function palindromeArray(str) {
+    var substrings = [];
 
-        for (var i = 0; i < str.length; i++) {
-            for (var j = 0; j < str.length-i; j++) {
-                let subStr = str.substring(j, j+i+3);
-                if (isPalindrome(subStr)) {
-                    substrings.push(subStr);
-                }
-            }
+    for (var i = 0; i < str.length; i++) {
+      for (var j = 0; j < str.length - i; j++) {
+        let subStr = str.substring(j, j + i + 3);
+        if (isPalindrome(subStr)) {
+          substrings.push(subStr);
         }
-        var subCount = substrings.filter(x => x.length >= 3).map(x => x.length);
-        var count = subCount.reduce((a, b) => a + b, 0)
-        if (count >= 1 && count <= 10) {
-            return "Possible";
-        }
-        if (count >= 11 && count <= 40) {
-            return "Probable";
-        }
-        if (count >= 41 && count <= 150) {
-            return "Escalate";
-        }
+      }
     }
-
-    function printArray(str) {
-        var symbols = str.split("").slice(str.length - 3, str.length).join("");
-        var palindromicText = str.toLowerCase().replace(/[\W_]/g, '');
-        return `${symbols} ${palindromeArray(palindromicText)}`
+    var subCount = substrings.filter(x => x.length >= 3).map(x => x.length);
+    var count = subCount.reduce((a, b) => a + b, 0)
+    if (count >= 1 && count <= 10) {
+      return "Possible";
     }
+    if (count >= 11 && count <= 40) {
+      return "Probable";
+    }
+    if (count >= 41 && count <= 150) {
+      return "Escalate";
+    }
+  }
 
-    console.log(textMessages.map(textMessage => printArray(textMessage)).join("\n"));
+  function printArray(str) {
+    var symbols = str.split("").slice(str.length - 3, str.length).join("");
+    var palindromicText = str.toLowerCase().replace(/[\W_]/g, '');
+    return `${symbols} ${palindromeArray(palindromicText)}`
+  }
+
+  console.log(textMessages.map(textMessage => printArray(textMessage)).join("\n"));
 }
 
 /**  Reverse string but maintain position of special characters*/
 function reverseChar(str) {
-    var specIndex = [];
-    var specChar = [];
-    for (var i = 0; i<str.length; i++) {
-        if (/[^a-zA-Z0-9]/g.test(str[i]) === true ) {
-            specIndex.push(i);
-            specChar.push(str[i]);
-        }
+  var specIndex = [];
+  var specChar = [];
+  for (var i = 0; i < str.length; i++) {
+    if (/[^a-zA-Z0-9]/g.test(str[i]) === true) {
+      specIndex.push(i);
+      specChar.push(str[i]);
     }
-    str = str.replace(/[^a-zA-Z0-9]/g,"").split("").reverse();
-    for (let i = 0; i<specChar.length; i++) {
-        str.splice(specIndex[i], 0, specChar[i])
-    }
-    console.log(str.join(""))
+  }
+  str = str.replace(/[^a-zA-Z0-9]/g, "").split("").reverse();
+  for (let i = 0; i < specChar.length; i++) {
+    str.splice(specIndex[i], 0, specChar[i])
+  }
+  console.log(str.join(""))
 }
 
 /** Another method of reversing string by Devin McIntyre */
 const reverser = str => {
-    //Create a map of special and non-special characters mapped to their index
-    const mapped = str
-        .split('')
-        .reduce((acc, character, index) => {
-            character.match(/^[0-9a-zA-Z]+$/) ? acc.letters.push(character) : acc.specials.push({character, index})
-            return acc
-        }, {letters: [], specials: []})
-    
-    const reversedArr = mapped.letters.reverse() //reverse the letters array
-    mapped.specials.forEach(special => reversedArr.splice(special.index, 0, special.character)) //add in the special characters at their index
-    return reversedArr.join("") //Return the string
+  //Create a map of special and non-special characters mapped to their index
+  const mapped = str
+    .split('')
+    .reduce((acc, character, index) => {
+      character.match(/^[0-9a-zA-Z]+$/) ? acc.letters.push(character) : acc.specials.push({ character, index })
+      return acc
+    }, { letters: [], specials: [] })
+
+  const reversedArr = mapped.letters.reverse() //reverse the letters array
+  mapped.specials.forEach(special => reversedArr.splice(special.index, 0, special.character)) //add in the special characters at their index
+  return reversedArr.join("") //Return the string
 }
 
 //Function to return array of object of character count
 function objCount(arr) {
-	const uniqueArray = [... new Set(arr)]
-	const mapped = uniqueArray
-        .reduce((acc, character, count) => {
-            count = numCount(arr,character);
-			acc.push({character, count});
-            return acc
-        }, [])
-	return mapped
+  const uniqueArray = [... new Set(arr)]
+  const mapped = uniqueArray
+    .reduce((acc, character, count) => {
+      count = numCount(arr, character);
+      acc.push({ character, count });
+      return acc
+    }, [])
+  return mapped
 }
 
 // Complete the MinSliceWeight function below.
 function MinSliceWeight(Matrix) {
-    // Get number of rows in Matrix i.e. Matrix[i]
-    var rows = Matrix.length;
-    // Get number of columns in Matrix i.e. Matrix[i][j]
-    var cols = Matrix.length > 0 ?  Matrix[0].length : 0;
-    // If rows or columns are 0, return 0
-    if (rows == 0 || cols == 0) {
-        return 0
+  // Get number of rows in Matrix i.e. Matrix[i]
+  var rows = Matrix.length;
+  // Get number of columns in Matrix i.e. Matrix[i][j]
+  var cols = Matrix.length > 0 ? Matrix[0].length : 0;
+  // If rows or columns are 0, return 0
+  if (rows == 0 || cols == 0) {
+    return 0
+  }
+
+  //Create slice sum array same length as columns
+  var sliceSum = Array(cols).fill(0);
+
+  //Fill slice sum array with values in last row
+  for (var i = 0; i < cols; i++) {
+    sliceSum[i] = !isNaN(Matrix[rows - 1][i]) ? Matrix[rows - 1][i] : 0
+  }
+
+  //Start iterating backwards from second-to-last row
+  for (var i = rows - 2; i >= 0; i--) {
+    //Iterate through columns backwards starting from last colum
+    for (var j = cols - 1; j >= 0; j--) {
+      //Detect non integer input
+      if (isNaN(Matrix[i][j])) {
+        Matrix[i][j] = 0
+      }
+      //If j is at last position, get minimum value of left-bottom element or immediate-bottom element and add to j
+      if (j == cols - 1) {
+        sliceSum[j] = Math.min(sliceSum[j - 1], sliceSum[j]) + Matrix[i][j];
+      }
+      //If j is at first position, get minimum value of right-bottom element or immediate-bottom element and add to j
+      else if (j == 0) {
+        sliceSum[j] = Math.min(sliceSum[j], sliceSum[j + 1]) + Matrix[i][j];
+      }
+      // Else if j is in the middle, get the minimum value of the the left, right or bottom element and add to j
+      else {
+        sliceSum[j] = Math.min(sliceSum[j - 1], sliceSum[j], sliceSum[j + 1]) + Matrix[i][j];
+      }
     }
+  }
 
-    //Create slice sum array same length as columns
-    var sliceSum = Array(cols).fill(0);
-
-    //Fill slice sum array with values in last row
-    for (var i = 0; i < cols; i++) {
-        sliceSum[i] = !isNaN(Matrix[rows - 1][i]) ? Matrix[rows - 1][i] : 0
-    }
-
-    //Start iterating backwards from second-to-last row
-    for (var i = rows - 2; i >= 0; i--) {
-        //Iterate through columns backwards starting from last colum
-        for (var j = cols - 1; j >= 0; j--) {
-            //Detect non integer input
-            if (isNaN(Matrix[i][j])) {
-                Matrix[i][j] = 0
-            }
-            //If j is at last position, get minimum value of left-bottom element or immediate-bottom element and add to j
-            if (j == cols - 1) {
-                sliceSum[j] = Math.min(sliceSum[j - 1], sliceSum[j]) + Matrix[i][j];
-            }
-            //If j is at first position, get minimum value of right-bottom element or immediate-bottom element and add to j
-            else if (j == 0) {
-                sliceSum[j] = Math.min(sliceSum[j], sliceSum[j + 1]) + Matrix[i][j];
-            }
-            // Else if j is in the middle, get the minimum value of the the left, right or bottom element and add to j
-            else {
-                sliceSum[j] = Math.min(sliceSum[j - 1], sliceSum[j], sliceSum[j + 1]) + Matrix[i][j];
-            }
-        }
-    }
-
-    //Sort sliceSum array in ascending order and return first value
-    return sliceSum.sort((a,b) => a -b)[0];
+  //Sort sliceSum array in ascending order and return first value
+  return sliceSum.sort((a, b) => a - b)[0];
 }
 
 // Complete the winner function below.
 function winner(erica, bob) {
-    erica = erica.split('');
-    bob = bob.split('');
+  erica = erica.split('');
+  bob = bob.split('');
 
-    function score(arr) {
-        var score = 0;
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i] == "S") {
-                score += 0;
-            }
-            if (arr[i] == "E") {
-                score += 1;
-            }
-            if (arr[i] == "M") {
-                score += 3;
-            }
-            if (arr[i] == "H") {
-                score += 5;
-            }
-        }
-        return score
+  function score(arr) {
+    var score = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] == "S") {
+        score += 0;
+      }
+      if (arr[i] == "E") {
+        score += 1;
+      }
+      if (arr[i] == "M") {
+        score += 3;
+      }
+      if (arr[i] == "H") {
+        score += 5;
+      }
     }
+    return score
+  }
 
-    function levelCount(arr, l) {
-        var count = 0;
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i] == l) {
-                count++;
-            }
-        }
-        return count
+  function levelCount(arr, l) {
+    var count = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] == l) {
+        count++;
+      }
     }
+    return count
+  }
 
-    var ericaScore = score(erica);
-    var bobScore = score(bob);
-    var ericaHScore = levelCount(erica, "H");
-    var bobHScore = levelCount(bob, "H");
-    var ericaMScore = levelCount(erica, "M");
-    var bobMScore = levelCount(bob, "M");
-    var ericaEScore = levelCount(erica, "E");
-    var bobEScore = levelCount(bob, "E");
+  var ericaScore = score(erica);
+  var bobScore = score(bob);
+  var ericaHScore = levelCount(erica, "H");
+  var bobHScore = levelCount(bob, "H");
+  var ericaMScore = levelCount(erica, "M");
+  var bobMScore = levelCount(bob, "M");
+  var ericaEScore = levelCount(erica, "E");
+  var bobEScore = levelCount(bob, "E");
 
-    if (ericaScore > bobScore) {
-        return "Erica"
-    } else if (bobScore > ericaScore ) {
-        return "Bob"
-    } else if (bobScore == ericaScore) {
-        if (ericaHScore > bobHScore) {
-            return "Erica"
-        } else if (ericaHScore < bobHScore) {
-            return "Bob"
-        } else if (ericaMScore > bobMScore) {
-            return "Erica"
-        } else if (ericaMScore < bobMScore) {
-            return "Bob"
-        } else if (ericaEScore > bobEScore) {
-            return "Erica"
-        } else if (ericaEScore < bobEScore) {
-            return "Bob"
-        } else {
-            return "Tie"
-        }
+  if (ericaScore > bobScore) {
+    return "Erica"
+  } else if (bobScore > ericaScore) {
+    return "Bob"
+  } else if (bobScore == ericaScore) {
+    if (ericaHScore > bobHScore) {
+      return "Erica"
+    } else if (ericaHScore < bobHScore) {
+      return "Bob"
+    } else if (ericaMScore > bobMScore) {
+      return "Erica"
+    } else if (ericaMScore < bobMScore) {
+      return "Bob"
+    } else if (ericaEScore > bobEScore) {
+      return "Erica"
+    } else if (ericaEScore < bobEScore) {
+      return "Bob"
+    } else {
+      return "Tie"
     }
+  }
 }
 
 function solution(A, K, L) {
-    if(K+L > A.length) {
-        return -1
+  if (K + L > A.length) {
+    return -1
+  }
+
+  function maximumSum(startIndex, num) {
+    var sum = 0;
+    for (var i = startIndex; i < num; i++) {
+      sum += A[i];
     }
-    
-    function maximumSum(startIndex, num) {
-        var sum = 0;
-        for(var i = startIndex; i < num; i++) {
-            sum += A[i];
-        }
-        var newSum = sum;
-        for(var i = num; i < A.length; i++) {
-            newSum += A[i] - A[i-num];
-            sum = Math.max(sum, newSum);
-        }
-        return sum
+    var newSum = sum;
+    for (var i = num; i < A.length; i++) {
+      newSum += A[i] - A[i - num];
+      sum = Math.max(sum, newSum);
     }
-    
-    var maxK = maximumSum(A, K);
-    var maxL = maximumSum(A, L);
-    return maxK+maxL
+    return sum
+  }
+
+  var maxK = maximumSum(A, K);
+  var maxL = maximumSum(A, L);
+  return maxK + maxL
 }
 
 // Return array of prime numbers in a number
 function primeFactorArray(number) {
-    var primes = [];
+  var primes = [];
 
-    //initialize divisor as lowest prime number, 2
-    for (var divisor = 2; number > 1; divisor ++) {
-        for(; number%divisor === 0; number/=divisor) {
-            primes.push(divisor);
-        }
+  //initialize divisor as lowest prime number, 2
+  for (var divisor = 2; number > 1; divisor++) {
+    for (; number % divisor === 0; number /= divisor) {
+      primes.push(divisor);
     }
+  }
 
-    return primes
+  return primes
+}
+
+function fizzBuzz(len) {
+  var arr = [];
+  for (var i = 1; i <= len; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz")
+    }
+    else if (i % 3 === 0) {
+      arr.push("Fizz")
+    }
+    else if (i % 5 === 0) {
+      arr.push("Buzz")
+    }
+    else {
+      arr.push(i)
+    }
+  };
+}
+
+module.exports.primeFactorArray = primeFactorArray;
+
+function translatePigLatin(str) {
+  if ((/^[aeiou]/).test(str)) {
+    return str + "way";
+  } else {
+    var strArr = str.split("");
+    var firstLetters = str.match(/^([^aeiou])+/g).join("");
+    str.splice(0, firstLetters.length);
+    str = strArr.join("") + firstLetters + "ay";
+    return str;
+  }
 }
